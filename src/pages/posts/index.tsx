7d6d76b34db6fbf3c051dc7017a98f887da06674
vitalyvitmens <otm_@tut.by>
2024-05-07 10:55:19 +0300

    feat: добавляет навигацию и компонент Link с useRouter()

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,40 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
+const links = [
+  {
+    href: '/posts/cars',
+    title: 'Машинки',
+  },
+  {
+    href: '/posts/cats',
+    title: 'Котятки',
+  },
+]
 export default function HelloPage() {
-  return <div>POSTS/INDEX</div>
+  return links.map((link) => (
+    <React.Fragment key={link.href}>
+      <Link href={link.href}>{link.title}</Link>
+      <br />
+    </React.Fragment>
+  ))
 }
+
+//! Так работать с ссылками не стоит. Пример использууется для ознакомления работы с кнопками как подмена ссылки 
+// export default function HelloPage() {
+//   const router = useRouter()
+
+//   return links.map((link) => {
+//     const handleClick = () => {
+//       router.push(link.href)
+//     }
+
+//     return (
+//       <React.Fragment key={link.href}>
+//         <button onClick={handleClick}>{link.title}</button>
+//         <br />
+//       </React.Fragment>
+//     )
+//   })
+// }
